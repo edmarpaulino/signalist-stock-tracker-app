8853cffbd2bd3a0104e470f201b91c606449504d
Edmar Paulino <edmarpaulino9@gmail.com>
2025-10-02 09:21:16 -0300

    Initial commit from Create Next App

---
+++ global.d.ts
@@ -0,0 +1,14 @@
+declare module '*.css' {
+  const content: unknown
+  export default content
+}
+
+declare module '*.scss' {
+  const content: unknown
+  export default content
+}
+
+declare module '*.sass' {
+  const content: unknown
+  export default content
+}
